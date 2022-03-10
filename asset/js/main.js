aad9db9cfcc5f54adfbdc60af3881ed33ef30c99
Ga <gabri.magni.97@gmail.com>
2022-03-10 17:34:50 +0100

    clear the code

--- asset/js/main.js
+++ asset/js/main.js
@@ -32,31 +32,3 @@ generateBtn.addEventListener("click", function () {
     document.getElementById("cp-number").append(`${randomCpCode}`);
     document.getElementById("ticket-cost").append(`${totPriceRounded} euro`);
 })
-
-
-
-
-
-// let mainContainer = document.getElementById("main-container");
-
-// let kmQuery = Number(prompt("Benvenuto sul sito, inserisca quanti kilometri deve percorrere:"));
-// let ageQuery = Number(prompt("Inserisca la sua età in anni:"));
-
-
-// let totPrice = kmQuery * 0.21;
-
-// if (ageQuery < 18) {
-//     totPrice -= (totPrice * 0.2);
-// } else if (ageQuery > 65) {
-//     totPrice -= (totPrice * 0.4);
-// } else {
-//     totPrice = totPrice
-// }
-
-// totPriceRounded = Math.round((totPrice + Number.EPSILON) * 100) / 100;
-
-// if (isNaN(kmQuery) || isNaN(ageQuery)) {
-//     mainContainer.innerHTML = `<p>ERRORE, ricaricare la pagina e inserire correttamente i dati</p>`
-// } else {
-//     mainContainer.innerHTML = `<p>Prezzo biglietto: ${totPriceRounded} &euro;</p>`
-// }
